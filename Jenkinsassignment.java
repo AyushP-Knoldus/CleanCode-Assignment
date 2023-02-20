9e056c15a3b790ea49a9d2d56833e3176a599197
AyushP-Knoldus <125042596+AyushP-Knoldus@users.noreply.github.com>
2023-02-20 23:10:02 +0530

    Update Jenkinsassignment.java

--- Jenkinsassignment.java
+++ Jenkinsassignment.java
@@ -2,6 +2,6 @@ public class Jenkinsassignment
 {
   public static void main(String [] args)
   {
-    System.out.println("Sample ASsignment");
+    System.out.println("Jenkins Assignment Completed.");
   }
 }
